94d9dfb0fae12abc9d0769dbeb30ef160dfdf40d
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:08:43 -0300

    Update and rename Ejercicio 1.html to Ejercicio -1.js

---
+++ modulo01/tp1/Ejercicio -1.js
@@ -0,0 +1,7 @@
+
+/*impresion basica con javaScript*/
+document.writeln("Hola Mundo!");
+/*Esta cosa imprime en pantalla lo que esta entre comillas*/
+document.write("Soy el primer script, de muchos");
+
+      
