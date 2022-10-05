deb77a69d3480091a67fb14e34807d054264ba49
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 09:57:08 -0300

    Create Ejercicio-6.js

---
+++ modulo01/tp2/Ejercicio-6.js
@@ -0,0 +1,13 @@
+        var e = prompt("introuce palabra", String)
+        vocalPrimera(e)
+
+        function vocalPrimera(e) {
+        
+            var vocales = ["a", "e", "i", "o", "u"];
+            for (let i = 0; i <= e.length; i++) {
+                if (vocales.includes(e[i])) {
+                    console.log("La primera vocal es " + e[i] + " y Está en la posicion " + (i + 1) + ".");
+                    i = (e.length + 1);
+                }
+            } 
+        }
