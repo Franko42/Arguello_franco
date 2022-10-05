06eaf1870f01c964cea410ba9e19fc202a1106c4
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:02:59 -0300

    Update Ejercicio-1.js

--- modulo01/tp2/Ejercicio-1.js
+++ modulo01/tp2/Ejercicio-1.js
@@ -1,32 +1,30 @@
- var verdadero = "verdadero"
-        var char = "char"
-        var Flag = 0
-        var mayor = 0
+var verdadero = "verdadero"
+var char = "char"
+var Flag = 0
+var mayor = 0
 
-        var valores = [verdadero, false, 2, "hola", "mundo", 3, char];
+var valores = [verdadero, false, 2, "hola", "mundo", 3, char];
 
-        var Val = [verdadero, valores[1].toString(), valores[2].toString(), "hola", "mundo", valores[5].toString(), char];
+var Val = [verdadero, valores[1].toString(), valores[2].toString(), "hola", "mundo", valores[5].toString(), char];
 
-        var valores = Val
+var valores = Val
 
-        for (i = 0; i < 7; i++) {
-            mayor = 0
-            Flag = valores[i].lenght
+for (i = 0; i < 7; i++) {
+     mayor = 0
+     Flag = valores[i].lenght
 
-            if (mayor = 0) {
-                mayor = flag
-            }
+     if (mayor = 0) {
+         mayor = flag
+      }
+      else if (Flag > mayor) {
+         mayor = Flag
+         }
+       }
 
+console.log("El mayor es  : ", valores[mayor])
 
-            else if (Flag > mayor) {
-                mayor = Flag
-            }
-        }
-
-        console.log("El mayor es  : ", valores[mayor])
-
-        console.log("ordenados", valores[2], valores[5], valores[6], valores[3], valores[4], valores[1], valores[0])
-        console.log("suma", valores[2] + valores[5])
-        console.log("resta", valores[2] - valores[5])
-        console.log("division", valores[2] / valores[5])
-        console.log("multiplicaion", valores[2] * valores[5])
+console.log("ordenados", valores[2], valores[5], valores[6], valores[3], valores[4], valores[1], valores[0])
+console.log("suma", valores[2] + valores[5])
+console.log("resta", valores[2] - valores[5])
+console.log("division", valores[2] / valores[5])
+console.log("multiplicaion", valores[2] * valores[5])
