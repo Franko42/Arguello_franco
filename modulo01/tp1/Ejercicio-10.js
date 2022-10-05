20a70a392535284328c3d219886bae3f53a80268
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:09:16 -0300

    Update and rename Ejercicio 10.html to Ejercicio-10.js

---
+++ modulo01/tp1/Ejercicio-10.js
@@ -0,0 +1,6 @@
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ];
+
+mes = window.prompt("Introduce un Número del 1 al 12", "");
+document.write("<h1>", meses[mes - 1], "</h1>");
+
