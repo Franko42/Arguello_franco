8454e509784b157fa366d51e35656eff66cc3297
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:00:58 -0300

    Create Ejercicio-9.js

---
+++ modulo01/tp2/Ejercicio-9.js
@@ -0,0 +1,19 @@
+ var notas = [3, 5 ,5 ,6, 7, 8]
+
+ var suma = 3 + 5 + 2 + 4 + 7 + 8 
+ 
+ var promedio = suma / 6
+        
+promedio = parseInt(promedio)
+        
+        
+        
+if (promedio < 5){
+   document.write("Reprobado")
+}
+else if(5<promedio>9){
+   document.write("Aprobado")
+ }     
+else if (promedio > 8){
+   document.write("Sobresaliente")
+}
