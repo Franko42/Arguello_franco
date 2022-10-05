0c8fa7e21609747ecfae09f6f075274118244ec6
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:05:25 -0300

    Update Ejercicio-4.js

--- modulo01/tp2/Ejercicio-4.js
+++ modulo01/tp2/Ejercicio-4.js
@@ -1,10 +1,10 @@
-        let n = prompt("Introduzca valor", 0)
-        parimpar(n)
-        function parimpar (n) {
-            if (n % 2 == 0 ){
-                console.log("numero par")
-            }
-            else {
-                console.log("numero impar")
-            }
-        }
+let n = prompt("Introduzca valor", 0)
+parimpar(n)
+function parimpar (n) {
+     if (n % 2 == 0 ){
+         console.log("numero par")
+         }
+     else {
+         console.log("numero impar")
+         }
+}
