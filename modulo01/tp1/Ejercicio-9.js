0e3b7c53d438a30342bf504b706c8dd687a08db2
Franco A. Argüello <113145536+Franko42@users.noreply.github.com>
2022-10-05 10:12:23 -0300

    Update and rename Ejercicio 9.html to Ejercicio-9.js

---
+++ modulo01/tp1/Ejercicio-9.js
@@ -0,0 +1,4 @@
+
+/*Hay formas menos repetitivas, pero lo hice asi */
+var Vocales = ["a", "e", "i", "o", "u"];
+document.write(Vocales[0], "<br>", Vocales[1], "<br>" , Vocales[2], "<br>", Vocales[3], "<br>", Vocales[4]);
